d0d23fd45e8ba4e50dd4959a716d929dcb13cae9
Noel Odero <anoel.odero@gmail.com>
2025-01-13 05:47:53 +0200

    tests

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,28 +1,40 @@
 #!/usr/bin/python3
+
+
+"""Unittest for State Class."""
+
 import unittest
+
 from models.state import State
+
 from models.base_model import BaseModel
 
-class TestUser(unittest.TestCase):
-    """Test cases for State class"""
-    
-    def setUp(self):
-        """Set up test method"""
-        self.state = State()
-        
-    def test_inheritance(self):
-        """Test if state inherits from BaseModel"""
-        self.assertIsInstance(self.state, BaseModel)
-        
-    def test_attributes(self):
-        """Test state attributes"""
-        self.assertTrue(hasattr(self.state, "state"))
-        
-        
-    def test_attributes_types(self):
-        """Test if attributes are strings"""
-        self.assertIsInstance(self.state.name, str)
-        
 
-if __name__ == '__main__':
+class TestState(unittest.TestCase):
+    """Test cases State class."""
+
+    def test_instance(self):
+        """test instance."""
+        state = State()
+        self.assertIsInstance(state, State)
+
+    def test_is_class(self):
+        """test instance."""
+        state = State()
+        self.assertEqual(str(type(state)),
+                         "<class 'models.state.State'>")
+
+    def test_is_subclass(self):
+        """test is_subclass."""
+        state = State()
+        self.assertTrue(issubclass(type(state), BaseModel))
+
+    def test_attr(self):
+        """test is_subclass."""
+        state = State()
+        self.assertIsNotNone(state.id)
+        self.assertEqual(state.name, "")
+
+
+if __name__ == "__main__":
     unittest.main()
