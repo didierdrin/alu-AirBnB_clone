5cc63732c30904aa3915a228e1b9569293037f1f
Noel Odero <anoel.odero@gmail.com>
2025-01-14 18:54:00 +0200

    pep compliance

--- models/place.py
+++ models/place.py
@@ -1,15 +1,16 @@
-from models.base_model import BaseModel 
+from models.base_model import BaseModel
+
 
 class Place(BaseModel):
     """Place class it inherits from BaseModel"""
     city_id = ""
     user_id = ""
     name = ""
-    description = "" 
+    description = ""
     number_rooms = 0
-    number_bathrooms = 0 
-    max_guest = 0 
-    price_by_night = 0 
-    latitude = 0.0 
-    longitude = 0.0 
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
     amenity_ids = []
